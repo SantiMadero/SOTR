9cb980a41a7a97e7f40703d802f5ea06be0af1d7
SantiMadero <santiago.madero-s@mail.escuelaing.edu.co>
2025-08-29 22:09:23 -0500

    Add typedefs for various data types in Datatype.h

--- Datatype.h
+++ Datatype.h
@@ -1,1 +1,30 @@
-Prueba
+typedef signed char char8;
+typedef unsigned char uchar8;
+typedef uchar8* puchar8;
+typedef char8* pchar8;
+
+typedef signed short short16;
+typedef unsigned short ushort16;
+typedef short16* pshort16;
+typedef ushort16* pushort16;
+
+typedef signed int int32;
+typedef unsigned int uint32;
+typedef int32* pint32;
+typedef uint32* puint32;
+
+typedef signed long long32;
+typedef unsigned long ulong32;
+typedef long32* plong32;
+typedef ulong32* pulong32;
+
+typedef signed long long dlong64;
+typedef unsigned long long udlong64;
+typedef dlong64* pdlong64;
+typedef udlong64* pudlong64;
+
+typedef float float32;
+typedef float32* pfloat32;
+
+typedef double double64;
+typedef double64* pdouble64; 
